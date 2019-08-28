6b1e69115b9d09640cb9a42b2afbe65ea53e7b85
ylja07 <yson@avans.nl>
2019-08-28 14:39:39 +0200

    sqfddadsfadsfadsadsfadscads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,7 @@
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
         </form> -->
+        <a href="{{ route('logout') }}"><button class="btn btn-danger" > logout </button></a>
     </div>
 </nav>
 @if(isset(Auth::user()->id))
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
+Route::get('logout', 'Auth\LoginController@logout');
 
-Route::get('/items', 'ItemsController@index');
-Route::get('/locations', 'LocationsController@index');
-Route::get('/projects', 'ProjectsController@index');
-Route::get('/users', 'UsersController@index');
-Route::get('', 'ProjectsController@index');
 
 Auth::routes();
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/items', 'ItemsController@index');
+    Route::get('/locations', 'LocationsController@index');
+    Route::get('/projects', 'ProjectsController@index');
+    Route::get('/users', 'UsersController@index');
+    Route::get('', 'ProjectsController@index');
+    });
