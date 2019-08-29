f4e40296b6a63f5f03adbdd2bde0da2bea09a1ef
Rick <ricksieben9@hotmail.com>
2019-08-29 10:17:20 +0200

    resources -> borrow/purchaseresources

--- app/Http/Resources/purchaseResource.php
+++ app/Http/Resources/purchaseResource.php
@@ -14,6 +14,13 @@ class purchaseResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+//            'users' => $this->whenLoaded("users"),
+            'quantity' => $this->quantity,
+            'price' => $this->price
+        ];
     }
 }
