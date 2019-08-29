fcd04b06fa44a95bfd76f860775d6feaa9d914b0
Rick <ricksieben9@hotmail.com>
2019-08-29 10:32:17 +0200

    Requests updated (borrow/purchase)

--- app/Http/Requests/borrowUpdateRequest.php
+++ app/Http/Requests/borrowUpdateRequest.php
@@ -6,15 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class borrowUpdateRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
 
     /**
      * Get the validation rules that apply to the request.
@@ -24,7 +15,10 @@ class borrowUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            "name" => "sometimes",
+            "description" => "sometimes",
+            "quantity" => "sometimes",
+            "borrowedFrom" => "sometimes"
         ];
     }
 }
--- app/Http/Requests/purchaseUpdateRequest.php
+++ app/Http/Requests/purchaseUpdateRequest.php
@@ -6,15 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class purchaseUpdateRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
 
     /**
      * Get the validation rules that apply to the request.
@@ -24,7 +15,10 @@ class purchaseUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            "name" => "sometimes",
+            "description" => "sometimes",
+            "quantity" => "sometimes",
+            "price" => "sometimes",
         ];
     }
 }
